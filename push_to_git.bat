6345abf51fcceb1b15d87ffde550099f8fb35f94
aki2025 <mylogindeatil2025@gmail.com>
2025-03-13 15:01:01 +0530

    Initial commit of AgentForge project

--- push_to_git.bat
+++ push_to_git.bat
@@ -6,6 +6,7 @@ git add .
 git commit -m "Initial commit of AgentForge project"
 git remote add origin https://github.com/aki2025/agentforge.git 2>nul
 if errorlevel 1 (echo Remote already exists, skipping...) else (echo Added remote repository)
+git branch -m master main
 git push -u origin main
 echo Done!
-pause 
+pause
